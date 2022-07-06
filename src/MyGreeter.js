37b302f526e32902bfd2f2497443942ccffc83bf
codertan <414384153@qq.com>
2022-07-06 20:47:57 +0800

    feat: 完善 `getGreeting`方法；完善测试用例；

--- src/MyGreeter.js
+++ src/MyGreeter.js
@@ -1,18 +1,67 @@
 
 class Client {
     constructor() {}
-    getGreeting() {
-        var date = new Date();
-        var hour = date.getHours();
+
+    /**
+     * 根据所传字符获取对应的问候语，timeStr为空时会自动计算当前时间
+     * @param timeStr: String Type，如 `06:40am` 、`12:01pm`
+     * @return 对应问候语 or 异常信息
+    */
+    getGreeting(timeStr) {
+
+        // 默认计算当前时间
+        if (typeof timeStr == 'undefined') {
+            return this.defaultGreeting();
+        }
+        
+        if (typeof timeStr !== 'string') {
+            return 'invalid parameter';
+        }
+
+        const regex = new RegExp(/^([0-1]\d):([0-5]\d)(?:AM|PM)?$/i);
+        timeStr = timeStr.toLowerCase();
+        if (!regex.test(timeStr)) {
+            return 'invalid parameter';
+        }
+        
+        const hour = parseInt(timeStr.split(':')[0]);
         var greeting = '';
-        if (hour > 6 && hour <= 12) {
+        if (timeStr.endsWith('am')) {
+            
+            if (hour >= 0 && hour < 6) {
+                greeting = 'Good evening';
+            }else if (hour >= 6 && hour < 12) {
+                greeting = 'Good morning';
+            }else {
+                greeting = 'invalid parameter';
+            }
+
+        }else if (timeStr.endsWith('pm')) {
+            
+            if ((hour > 0 && hour < 6) || hour == 12) {
+                greeting = 'Good afternoon';
+            }else if (hour >= 6 && hour < 12) {
+                greeting = 'Good evening';
+            }else {
+                greeting = 'invalid parameter';
+            }
+        }else {
+            return 'invalid parameter';
+        }
+        return greeting;
+    }
+
+    // 获取当前时间下的问候语
+    defaultGreeting() {
+        const hour = new Date().getHours();
+        var greeting = '';
+        if (hour >= 6 && hour < 12) {
             greeting = "Good morning"
-        }else if(hour > 12 && hour <= 18) {
+        }else if(hour >= 12 && hour < 18) {
             greeting = "Good afternoon"
         }else {
             greeting = "Good evening"
         }
-        // console.log(greeting);
         return greeting;
     }
 }
--- test/my_greeter-client.js
+++ test/my_greeter-client.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var assert = require('assert');
-var MyGreeter = require('../src/MyGreeter.js');
+const assert = require('assert');
+const MyGreeter = require('../src/MyGreeter.js');
 
 
 describe('MyGreeter.Client', function () {
@@ -9,17 +9,60 @@ describe('MyGreeter.Client', function () {
     describe('Constructor', function () {
 
         it('should be instantiated', function() {
-            var greeter = new MyGreeter.Client();
+            const greeter = new MyGreeter.Client();
             assert.ok(greeter instanceof MyGreeter.Client);
         });
     });
 
     describe('getGreeting', function () {
 
-        it('should return something with length', function() {
-            var greeter = new MyGreeter.Client();
-
+        // 测试当前时间对应问候语
+        it('should return greeting at the moment', function() {
+            const greeter = new MyGreeter.Client();
             assert.equal(greeter.getGreeting().length > 0, true);
+            
+            const now = new Date().getHours();
+            if (now >= 6 && now < 12) {
+                assert.equal(greeter.getGreeting(), 'Good morning');
+            }else if (now >= 12 && now < 18) {
+                assert.equal(greeter.getGreeting(), 'Good afternoon');
+            }else {
+                assert.equal(greeter.getGreeting(), 'Good evening');
+            }
+
         });
+
+        // 测试非法参数
+        it('should return `invalid parameter`', function() {
+            const greeter = new MyGreeter.Client();
+            assert.equal(greeter.getGreeting(12), 'invalid parameter'); // not string
+            assert.equal(greeter.getGreeting('fsafffa'), 'invalid parameter'); // not match regex like `00:00am`
+            assert.equal(greeter.getGreeting('12:01am'), 'invalid parameter'); // 12:01 is pm
+            assert.equal(greeter.getGreeting('00:01pm'), 'invalid parameter'); // 00:01 is am
+        });
+
+        // 测试`Good morning`区间 [6,12)
+        it('should return `Good morning`', function() {
+            const greeter = new MyGreeter.Client();
+            assert.equal(greeter.getGreeting('06:00am'), 'Good morning');
+            assert.equal(greeter.getGreeting('11:59am'), 'Good morning');
+        });
+
+        // 测试`Good afternoon`区间 [12,18)
+        it('should return `Good afternoon`', function() {
+            const greeter = new MyGreeter.Client();
+            assert.equal(greeter.getGreeting('12:00pm'), 'Good afternoon');
+            assert.equal(greeter.getGreeting('05:59pm'), 'Good afternoon');
+        });
+
+        // 测试`Good evening`区间 [18, 24) & [0, 6)
+        it('should return `Good evening`', function() {
+            const greeter = new MyGreeter.Client();
+            assert.equal(greeter.getGreeting('06:00pm'), 'Good evening');
+            assert.equal(greeter.getGreeting('11:59pm'), 'Good evening');
+            assert.equal(greeter.getGreeting('00:00am'), 'Good evening');
+            assert.equal(greeter.getGreeting('05:59am'), 'Good evening');
+        });
+
     });
 });
